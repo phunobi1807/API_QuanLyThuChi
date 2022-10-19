129dcd2d33d7ed1c95c95c3d514a00c505d7e23f
DESKTOP-IMPD0F1\admin <phunobi1807@gmail.com>
2022-10-19 17:22:13 +0700

    reser password

--- controllers/Users.js
+++ controllers/Users.js
@@ -3,11 +3,11 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 import { Sequelize, Op, NUMBER } from "sequelize";
- 
-export const getUsers = async(req, res) => {
+
+export const getUsers = async (req, res) => {
     try {
         const users = await Users.findAll({
-            attributes:['id','code_number','fullname', 'phone_number', 'email', 'status']
+            attributes: ['id', 'code_number', 'fullname', 'phone_number', 'email', 'status']
         });
         res.json(users);
     } catch (error) {
@@ -15,91 +15,101 @@ export const getUsers = async(req, res) => {
     }
 }
 
-export const getUserByCodeNumber = async(req, res) =>{
-    const code_number = req.query.code_number;
-    const phone_number = req.query.phone_number;
+export const getUserByPhone = async (req, res) => {
+    const phone_number = req.params.phone_number;
     Users.findAll({
         where: {
-            [Op.or]: [{
-                code_number: {
-                        [Op.like]: "%code_number"
-                    }
-                },
+            [Op.and]: [
                 {
                     phone_number: {
-                        [Op.like]: "%phone_number"
+                        [Op.like]: "%" + phone_number + "%"
                     }
                 }
             ]
         }
     })
-    .then(data => {
-        res.send(data);
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving tutorials."
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving tutorials."
+            });
         });
-    });
+}
+export const getUserByCodeNumber = async (req, res) => {
+    const code_number = req.params.code_number; 
+    Users.findAll({
+        where: {
+            [Op.and]: [
+                {
+                    code_number: {
+                        [Op.like]: "%" + code_number + "%"
+                    }
+                }
+            ]
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
 }
 
-// export const getUserByCodeNumber = async(req, res) =>{
-//     const i = Number
-//     try {
-//         const response = await Users.findOne({
-//             where:{
-//                 phone_number: +req.params.phone_number[i]
-//             }
-//         });
-//         res.status(200).json(response);
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+export const getUserByEmail = async (req, res) => {
+    const email = req.params.email; 
+    Users.findAll({
+        where: {
+            [Op.and]: [
+                {
+                    email: {
+                        [Op.like]: "%" + email + "%"
+                    }
+                }
+            ]
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
+}
 
-// export const getUserById = async (req, res) => {
-//     try {
-//         const user = await Users.findOne({
-//             where: {
-//                 id: req.params.id,
-//                 // code_number: req.params.code_number,
-//                 // phone_numer: req.params.phone_numer,
-//                 // email: req.params.email
-//             }
-//         });
-//         res.json(user);
-//     } catch(error) {
-//         res.json({ message: error.message });
-//     }
-// }
-// export const getUserByEmail = async (req, res) => {
-//     try {
-//         const user = await Users.findAll({
-//             where: {
-//                 email: req.params.email,
-//             }
-//         });
-//         res.json(user[0]);
-//     } catch(error) {
-//         res.json({ message: error.message });
-//     }
-// }
-// export const getUserByName = async (req, res) => {
-//     try {
-//         const user = await Users.findAll({
-//             where: {
-//                 fullname: req.params.fullname,
-//             }
-//         });
-//         res.json(user[0]);
-//     } catch(error) {
-//         res.json({ message: error.message });
-//     }
-// }
+export const getUserByFullName = async (req, res) => {
+    const fullname = req.params.fullname; 
+    Users.findAll({
+        where: {
+            [Op.and]: [
+                {
+                    fullname: {
+                        [Op.like]:"%" + fullname + "%"
+                    }
+                }
+            ]
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
+}
 
-export const Register = async(req, res) => {
-    const { code_number, fullname, phone_number ,email , password, confPassword } = req.body;
-    if(password !== confPassword) return res.status(400).json({msg: "Password and Confirm Password do not match"});
+
+export const Register = async (req, res) => {
+    const { code_number, fullname, phone_number, email, password, confPassword } = req.body;
+    if (password !== confPassword) return res.status(400).json({ msg: "Password and Confirm Password do not match" });
     const salt = await bcrypt.genSalt();
     const hashPassword = await bcrypt.hash(password, salt);
     try {
@@ -110,60 +120,61 @@ export const Register = async(req, res) => {
             phone_number: phone_number,
             password: hashPassword
         });
-        res.json({msg: "Register Successful"});
+        res.json({ msg: "Register Successful" });
     } catch (error) {
         console.log(error);
     }
 }
 
-export const Login = async(req, res) => {
+export const Login = async (req, res) => {
     try {
         const user = await Users.findAll({
-            where:{
+            where: {
                 code_number: req.body.code_number
             }
         });
         const match = await bcrypt.compare(req.body.password, user[0].password);
-        if(!match) return res.status(400).json({msg: "Wrong Password"});
+        if (!match) return res.status(400).json({ msg: "Wrong Password" });
         const userId = user[0].id;
         const fullname = user[0].fullname;
         const userCode = user[0].code_number;
-        const accessToken = jwt.sign({userId, fullname, userCode}, process.env.ACCESS_TOKEN_SECRET,{
+        const accessToken = jwt.sign({ userId, fullname, userCode }, process.env.ACCESS_TOKEN_SECRET, {
             expiresIn: '15s'
         });
-        const refreshToken = jwt.sign({userId, fullname, userCode}, process.env.REFRESH_TOKEN_SECRET,{
+        const refreshToken = jwt.sign({ userId, fullname, userCode }, process.env.REFRESH_TOKEN_SECRET, {
             expiresIn: '1d'
         });
-        await Users.update({refresh_token: refreshToken},{
-            where:{
+        await Users.update({ refresh_token: refreshToken }, {
+            where: {
                 id: userId
             }
         });
-        res.cookie('refreshToken', refreshToken,{
+        res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000
         });
         res.json({ accessToken });
     } catch (error) {
-        res.status(404).json({msg:"User code not true"});
+        res.status(404).json({ msg: "User code not true" });
     }
 }
- 
-export const Logout = async(req, res) => {
+
+export const Logout = async (req, res) => {
     const refreshToken = req.cookies.refreshToken;
-    if(!refreshToken) return res.sendStatus(204);
+    if (!refreshToken) return res.sendStatus(204);
     const user = await Users.findAll({
-        where:{
+        where: {
             refresh_token: refreshToken
         }
     });
-    if(!user[0]) return res.sendStatus(204);
+    if (!user[0]) return res.sendStatus(204);
     const userId = user[0].id;
-    await Users.update({refresh_token: null},{
-        where:{
+    await Users.update({ refresh_token: null }, {
+        where: {
             id: userId
         }
     });
     res.clearCookie('refreshToken');
     return res.sendStatus(200);
 }
+
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { refreshToken } from "../controllers/RefreshToken";
-import { getUserByCodeNumber, getUsers, Login, Logout, Register } from "../controllers/Users";
+import { getUserByCodeNumber, getUserByEmail, getUserByFullName, getUserByPhone, getUsers, Login, Logout, Register } from "../controllers/Users";
 import { verifyToken } from "../middleware/VerifyToken";
 
 const router = express.Router();
 
 router.get("/users" ,getUsers);
-router.get("/users/:id", getUserByCodeNumber); // HERE
+router.get("/users/phoneNumber/:phone_number", getUserByPhone); // HERE
+router.get("/users/codeNumber/:code_number", getUserByCodeNumber); // HERE
+router.get("/users/searchEmail/:email", getUserByEmail); // HERE
+router.get("/users/searchFullName/:fullname", getUserByFullName); // HERE
 router.post('/users', Register);
+// router.post('/users/reset_password', ResetPassword);
 router.post("/login", Login);
 router.get("/token", refreshToken);
 router.delete("/logout", Logout);
